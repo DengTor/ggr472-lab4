24fb62dd734a851f47b95bcb1d35fe26508f57cd
DengTor <abraham.tor@mail.utoronto.ca>
2023-03-23 16:24:18 -0400

    update

--- script.js
+++ script.js
@@ -14,10 +14,8 @@ const map = new mapboxgl.Map({
     container: 'map', //container id in HTML
     style: 'mapbox://styles/dengtor/clexm94ej000u01porlynijpv',  //****ADD MAP STYLE HERE *****
     center: [-79.39, 43.65],  // starting point, longitude/latitude
-    zoom: 12 // starting zoom level
+    zoom: 9 // starting zoom level for the map
 });
-
-
 
 /*--------------------------------------------------------------------
 Step 2: VIEW GEOJSON POINT DATA ON MAP
@@ -26,17 +24,82 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 //      Use the fetch method to access the GeoJSON from your online repository
 //      Convert the response to JSON format and then store the response in your new variable
 
+let degeojson; // empty variable
 
+// Fetch GeoJSON from URL and store response
+fetch('https://raw.githubusercontent.com/DengTor/ggr472-lab4/main/data/pedcyc_collision_06-21.geojson')
+    .then(response => response.json())
+    .then(response => {
+        console.log(response); //Check response in console
+        degeojson = response; // Store geojson as variable using URL from fetch response
+        console.log(response)
+    });
 
+// /*--------------------------------------------------------------------
+// LOAD DATA TO MAP USING GEOJSON VARIABLE
+// --------------------------------------------------------------------*/
+map.on('load', () => {
+
+    //Add datasource using GeoJSON variable
+    map.addSource('collision', {
+        type: 'geojson',
+        data: degeojson
+        
+    });
+
+    map.addLayer({
+        'id': 'collision-pnts',
+        'type': 'circle',
+        'source': 'collision',
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': 'blue'
+        }
+    });
+
+});
 /*--------------------------------------------------------------------
     Step 3: CREATE BOUNDING BOX AND HEXGRID
 --------------------------------------------------------------------*/
 //HINT: All code to create and view the hexgrid will go inside a map load event handler
-//      First create a bounding box around the collision point data then store as a feature collection variable
+
+let bboxgeojson; // This is an empty variable that will hold the new boundging box feature
+let bbox;
+
+document.getElementById('bboxbutton').addEventListener('click', () => {
+    let bbox = turf.envelope(degeojson); // send point geojson to turf, creates a bounding box around points
+    //put the resulting bounding box in geojson format FeatureCollection
+    bboxgeojson = {
+        "type": "FeatureCollection",
+        "features": [bbox]
+    };
+
+    //add the bounding box created to the map
+    map.addSource('collision-bbox', {
+        "type": "geojson",
+        "data": bboxgeojson.features[0], //use bbox geojson variable as data source
+        
+    });
+
+    map.addLayer({ //This enables viewing of the bounding box
+        "id": "bboxEnvelope",
+        "type": "fill",
+        "source": "collision-bbox",
+        "paint": {
+            'fill-color': "purple",
+            'fill-opacity': 0.5,
+            'fill-outline-color': "black"
+        }
+    });
+
+    document.getElementById('bboxbutton').disabled = true; //disable button after click 
+
+});
+
 //      Access and store the bounding box coordinates as an array variable
 //      Use bounding box coordinates as argument in the turf hexgrid function
 
-
+console.log(bbox.geometry.coordinates[0][0][0])
 
 /*--------------------------------------------------------------------
 Step 4: AGGREGATE COLLISIONS BY HEXGRID
@@ -55,5 +118,19 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 //        - The COUNT attribute
 //        - The maximum number of collisions found in a hexagon
 //      Add a legend and additional functionality including pop-up windows
+const legendlabels = [
+    '0-400',
+    '400-800',
+    '800-1000',
+    '1000-1200',
+    '>1200'
+];
 
+const legendcolours = [
+    '#800020',
+    '#A52A2A',
+    '#EE4B2B',
+    '#AA4A44',
+    '#880808',
 
+];
